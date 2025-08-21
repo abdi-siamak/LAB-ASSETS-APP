045fdd50981b3170be27b45a0581776c6576f298
siamakabdi <abdi.siamak@gmail.com>
2025-08-21 17:03:02 +0200

    some files are modified

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(morgan('dev')); // Morgan is a HTTP request logger middleware.
 app.use(cors({ origin: process.env.CORS_ORIGIN || '*' })); // Cross-Origin Resource Sharing. It controls which domains are allowed to make requests to your server from a different origin.
-app.use(express.json());
+app.use(express.json()); // express.json() is built-in middleware in Express. It parses incoming requests with JSON payloads and makes the data available on req.body
 
 app.get('/api/health', (req, res) => res.json({ok: true}));
 app.use('/api/assets', assetRoutes); // assetRoutes: route handler
@@ -25,6 +25,6 @@ connectDB(process.env.MONGODB_URI)
     .then(() => app.listen(PORT, () => console.log(`API listening on ${PORT}`)))
     .catch(err => {
         console.error('Mongo connection failed', err);
-        process.exit(1); // exit(0) → Indicates successful execution|exit(1) → Indicates an error or abnormal termination. 
+        process.exit(1); // exit(0) → Indicates successful execution| exit(1) → Indicates an error or abnormal termination. 
     });
 
